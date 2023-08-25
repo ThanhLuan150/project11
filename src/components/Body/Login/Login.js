186a27b409a8d5ed5c09052a8bfa909ea5a8f8aa
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-08-25 09:14:05 +0700

    main

--- src/components/Body/Login/Login.js
+++ src/components/Body/Login/Login.js
@@ -19,7 +19,7 @@ const Login = ()=>{
       e.preventDefault();
   
       try {
-        const response = await axios.post('http://127.0.0.1:8000/api/login', {
+        const response = await axios.post('http://127.0.0.1:8000/api/Login', {
           email,
           password,
         });
